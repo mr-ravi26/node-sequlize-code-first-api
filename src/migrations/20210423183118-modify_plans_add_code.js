f0e8d05a693c48a66ba5c8cea9c255306da80c31
Ravi B Tripathi <ravi@subtlelabs.com>
2021-04-24 00:16:52 +0530

    Code filed added to plan model, seed created

---
+++ src/migrations/20210423183118-modify_plans_add_code.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.addColumn(
+      'Plans', // table name
+      'code', // new field name
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
---
+++ src/seeders/20210423182753-Plan.js
@@ -0,0 +1,75 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Plans', [
+      {
+        id: 1,
+        code: 'FREE',
+        validity: 'INFINITE',
+        cost: 0.00,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        id: 2,
+        code: 'TRIAL',
+        validity: '7',
+        cost: 0.00,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        id: 3,
+        code: 'LITE_1M',
+        validity: '30',
+        cost: 100.00,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        id: 4,
+        code: 'PRO_1M',
+        validity: '30',
+        cost: 200.00,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        id: 5,
+        code: 'LITE_6M',
+        validity: '180',
+        cost: 500.00,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      },
+      {
+        id: 6,
+        code: 'PRO_6M',
+        validity: '180',
+        cost: 900.00,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
